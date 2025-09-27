e9d2259b6a2f6013fb519761c76f969d5c8d5374
Алекс Тырданов <alekstyrdanov@Alekss-Mac-mini.local>
2025-09-27 17:51:00 +0300

    #42 feat: add delete button in admin for articles

--- src/components/Admin/Article/ArticleTable.tsx
+++ src/components/Admin/Article/ArticleTable.tsx
@@ -1,10 +1,17 @@
 import GenericTable from '../GenericTable'
 import { observer } from 'mobx-react-lite'
 import { useStores } from '@/hooks/useStores'
+import { ArticleType } from '@/types/ArticleTypes'
+import ActionButtons from '../ActionButtons'
 
 const ArticleTable = () => {
     const { articleStore } = useStores()
     const articles = articleStore.getArticles()
+    const onDelete = async (article: ArticleType) => {
+        await articleStore.delete(article.id)
+        await articleStore.fetch()
+    }
+
     const columns = [
         { title: 'ID', dataIndex: 'id', key: 'id' },
         { title: 'Название', dataIndex: 'title', key: 'title' },
@@ -14,6 +21,17 @@ const ArticleTable = () => {
         { title: 'ID пользователя', dataIndex: 'user_id', key: 'user_id' },
         { title: 'ID категорий', dataIndex: 'category_ids', key: 'category_ids' },
         { title: 'ID изображений', dataIndex: 'image_ids', key: 'image_ids' },
+        {
+            title: 'Действия',
+            key: 'actions',
+            render: (_: any, record: ArticleType) => (
+                <ActionButtons<ArticleType>
+                    record={record}
+                    onDelete={onDelete}
+                    confirmDeleteMessage="Вы уверены, что хотите удалить статью?"
+                />
+            ),
+        },
     ]
 
     return <GenericTable dataSource={articles} columns={columns} />
